11e74c370a8e966bda5f68a5221d0d48266dc704
Muskan <mkmuskanbegam@gmail.com>
2025-08-11 23:19:33 +0530

    First Commit

---
+++ app.py
@@ -0,0 +1,93 @@
+from flask import Flask, render_template, redirect, url_for,request,session
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.secret_key = "muskanbegam"
+
+#config database
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///user.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATION"] = False
+
+#create database
+db = SQLAlchemy(app)
+
+#Database mODEL
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(25), unique= True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+    
+    def set_password(self,password):
+        self.password = generate_password_hash(password)
+        
+    def check_password(self,password):
+        return check_password_hash(self.password,password)
+    
+    
+@app.route('/')
+def home():
+    if 'email' in session:
+        return redirect('dashboard')
+    print('Session cleared')
+    return render_template("auth.html")
+
+@app.route('/auth/<page>', methods=['GET','POST'])
+def auth(page):
+    if request.method == "POST":
+        email = request.form['email']
+        password = request.form['password']
+        print(email,password)
+        user = User.query.filter_by(email=email).first()
+        print(user)
+
+        if page == "login" :
+            
+            if user and user.check_password(password):
+                session['email'] = email
+                return redirect(url_for('dashboard'))
+        else:
+            
+            if user:
+                return render_template("dashboard.html", error="User already exists")
+            else:
+                new_user = User(email = email)
+                new_user.set_password(password)
+                db.session.add(new_user)
+                db.session.commit()
+                session['email'] = email
+                return redirect('auth', page='login')  
+    return render_template('auth.html') 
+ 
+
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html', active_page='dashboard')
+
+@app.route('/feed')
+def feed():
+    return render_template('feed.html', active_page='feed')
+
+@app.route('/profile')
+def profile():
+    return render_template('profile.html', active_page='profile')
+
+@app.route('/tasks')
+def tasks():
+    return render_template('tasks.html', active_page='tasks')
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('home', active_page='logout'))
+
+     
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(host='0.0.0.0', debug=True)
+    
+    
+    
+           
